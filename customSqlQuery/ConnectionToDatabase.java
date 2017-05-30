35aec6e0df08ee9ebdac6ddfddc3fcd94d4b309c
Creta21 <giannisapokoronas13@gmail.com>
2017-05-30 14:27:44 +0300

    conection with properties file

--- customSqlQuery/ConnectionToDatabase.java
+++ customSqlQuery/ConnectionToDatabase.java
@@ -1,7 +1,12 @@
 package customSqlQuery;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.Properties;
 
 public class ConnectionToDatabase {
     public Connection connect() throws Exception{
@@ -21,4 +26,47 @@ public class ConnectionToDatabase {
         }
         return null;
     }
+    
+    /////////////////////////////////////////////////////////////////////////
+    //           Second way with application.properties file               //
+    /////////////////////////////////////////////////////////////////////////
+    public Connection connect2() throws Exception{
+        try {
+            ArrayList<String> prop = this.getProperties();
+            Connection conn = DriverManager.getConnection(prop.get(0), prop.get(1), prop.get(2));
+            return conn;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    ///// get properties from file.
+    private ArrayList<String> getProperties() throws IOException{
+        Properties prop = new Properties();
+	InputStream input = null;
+        ArrayList<String> properties = new ArrayList<>();
+	try {
+            // Take the file from Other Sources
+            input = new FileInputStream("src/main/resources/database.properties");
+            // load a properties file
+            prop.load(input);
+            // add to array list
+            properties.add(prop.getProperty("database"));
+            properties.add(prop.getProperty("dbuser"));
+            properties.add(prop.getProperty("dbpassword"));
+
+	} catch (IOException ex) {
+		ex.printStackTrace();
+	} finally {
+            if (input != null) {
+                try {
+                        input.close();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+            }
+	}
+        return properties;
+    } 
 }
